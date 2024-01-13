1ec420a05bfd3b5e2de39ce3aa1aff4612019470
HimuHimu112002 <mdhmaktaruzzaman9101@gmail.com>
2024-01-13 20:07:59 +0600

    news

---
+++ src/app/api/comments/newsByComment/route.js
@@ -0,0 +1,26 @@
+import {NextResponse} from "next/server";
+import {PrismaClient} from "@prisma/client";
+
+
+export async function GET(req,res) {
+    try{
+
+        const prisma=new PrismaClient();
+
+        let {searchParams} = new URL(req.url);
+        let postID = parseInt((searchParams.get('postID'))
+)
+        const result=await prisma.comments.findMany(
+            {
+                where:{postID: postID},
+                include:{
+                    users:{select:{firstName:true, lastName:true}}
+                }
+            }
+            )
+        return  NextResponse.json({status:"success",data:result})
+    }
+    catch (e) {
+        return  NextResponse.json({status:"fail",data:e.toString()})
+    }
+}
--- src/app/api/news/newsList/route.js
+++ src/app/api/news/newsList/route.js
@@ -4,7 +4,6 @@ import {PrismaClient} from "@prisma/client";
 export async function POST(req,res) {
     try{
         let reqBody = await req.json();
-        // console.log(reqBody)
         const prisma=new PrismaClient();
 
         const result = await prisma.news_list.create({data:reqBody})
